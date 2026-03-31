e6348532b5dbb802b9dab52851bd982af9d3e59d
יובל סיגל <yuval@MacBook-Air-sl-ywbl.local>
2026-03-31 16:47:48 +0300

    fixed router

--- app/App.js
+++ app/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 
+// ייבוא המסכים
 import LoginScreen from './src/screens/LoginScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
 import QuizStartScreen from './src/screens/QuizStartScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
-//test2
 export default function App() {
+  // הסרנו את ה-NavigationContainer כי Expo Router כבר מספק אחד ב-_layout
   return (
-    <NavigationContainer independent={true}> 
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="QuizStart" component={QuizStartScreen} />
-      </Stack.Navigator>
-     </NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="QuizStart" component={QuizStartScreen} />
+    </Stack.Navigator>
   );
-} 
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import App from "./App";
 
 export default function Index() {
-  // אנחנו פשוט מחזירים את App בלי שום View או Text מסביב
   return <App />;
 }
