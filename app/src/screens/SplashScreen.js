01659f17f56c01a47f96a720ea1f72c881fab75f
LielRobinov <liel63312@gmail.com>
2026-04-05 20:49:10 +0300

    הוספת לוגו לאפליקציה ולמסך Splash

--- app/src/screens/SplashScreen.js
+++ app/src/screens/SplashScreen.js
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { Animated, Image, StyleSheet, Text, View } from "react-native";
 
 export default function SplashScreen({ navigation }) {
   const fadeAnim = new Animated.Value(0); // אנימציית הופעה
@@ -14,7 +14,7 @@ export default function SplashScreen({ navigation }) {
 
     // אחרי 3 שניות, עובר אוטומטית למסך ה-Login
     const timer = setTimeout(() => {
-      navigation.replace('Login');
+      navigation.replace("Login");
     }, 3500);
 
     return () => clearTimeout(timer);
@@ -22,11 +22,17 @@ export default function SplashScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{ opacity: fadeAnim }}>
+      <Animated.View style={{ opacity: fadeAnim, alignItems: "center"}}>
+        <Image
+          source={require("../../../assets/images/onlyLogo-removebg-preview.png")}
+          style={{ width: 250, height: 250, marginBottom: 10 }}
+          resizeMode="contain"
+        />
+
         <Text style={styles.logoText}>צמד חמד</Text>
         <Text style={styles.subTitle}>החצי השני שלך לטיול הבא</Text>
       </Animated.View>
-      
+
       {/* אם יש לך לוגו ב-assets, תוכלי להוסיף אותו כאן */}
       <View style={styles.footer}>
         <Text style={styles.footerText}>מתחברים בקרוב...</Text>
@@ -38,31 +44,31 @@ export default function SplashScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1A3C40', // הצבע הירוק הכהה מהלוגו שלך
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#1A3C40", // הצבע הירוק הכהה מהלוגו שלך
+    justifyContent: "center",
+    alignItems: "center",
   },
   logoText: {
     fontSize: 60,
-    fontWeight: 'bold',
-    color: '#E0E7E9', // הצבע הבהיר של האפליקציה
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "#E0E7E9", // הצבע הבהיר של האפליקציה
+    textAlign: "center",
     letterSpacing: 2,
   },
   subTitle: {
     fontSize: 18,
-    color: '#E0E7E9',
-    textAlign: 'center',
+    color: "#E0E7E9",
+    textAlign: "center",
     marginTop: 10,
     opacity: 0.8,
   },
   footer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 50,
   },
   footerText: {
-    color: '#E0E7E9',
+    color: "#E0E7E9",
     fontSize: 14,
     opacity: 0.5,
-  }
+  },
 });
